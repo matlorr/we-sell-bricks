9bbaa276a4e1ddaa8da75d60db3d346e2ea6307f
matlorr <matvey.lorkish@outlook.de>
2020-02-21 16:57:54 +0100

    ADDED AND FIXED database gets initilized by application properties. Database schema renamed to 'bricks' password was changed to. Also minor gradle dependencies hanged.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,45 +2,42 @@ DROP TABLE IF EXISTS bestellung;
 
 CREATE TABLE bestellung
 (
-    bestellung integer PRIMARY KEY,
-    datum text,
-    kunde integer DEFAULT NULL
+    bestellnr integer PRIMARY KEY,
+    datum     text,
+    kunde     integer DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS kunde;
-
 CREATE TABLE kunde
 (
     kundennr integer PRIMARY KEY,
-    name text,
-    plz integer DEFAULT NULL,
-    stadt text,
-    strasse text,
-    lng double DEFAULT NULL,
-    lat double DEFAULT NULL
+    name     text,
+    plz      integer DEFAULT NULL,
+    stadt    text,
+    strasse  text,
+    lng      double  DEFAULT NULL,
+    lat      double  DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS positionen;
-
-CREATE TABLE positionen
+DROP TABLE IF EXISTS position;
+CREATE TABLE position
 (
-    id integer PRIMARY KEY,
-    bestellung integer REFERENCES bestellung(bestellnr),
-    anzahl integer DEFAULT NULL,
-    produkt integer DEFAULT NULL
-)
+    id         integer PRIMARY KEY,
+    bestellung_dto integer REFERENCES bestellung (bestellnr),
+    anzahl     integer DEFAULT NULL,
+    produkt    integer DEFAULT NULL
+);
 
 DROP TABLE IF EXISTS produkt;
-
 CREATE TABLE produkt
 (
-    id integer PRIMARY KEY;
-    gewicht text,
-    hoehe text,
-    breite text,
-    tiefe text,
-    preis text,
-    produkt text,
+    id           integer PRIMARY KEY,
+    gewicht      text,
+    hoehe        text,
+    breite       text,
+    tiefe        text,
+    preis        text,
+    produkt      text,
     beschreibung text,
-    bestand INTEGER
-);
+    bestand      INTEGER
+) ;
