8d7edfad67aac71556afec882f8d09fcf34b52e0
matlorr <matvey.lorkish@outlook.de>
2020-02-21 14:36:47 +0100

    initial commit

---
+++ src/main/java/de/bricks/brickmarket/BrickMarketApplication.java
@@ -0,0 +1,13 @@
+package de.bricks.brickmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrickMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/bricks/brickmarket/BrickMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bricks.brickmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
