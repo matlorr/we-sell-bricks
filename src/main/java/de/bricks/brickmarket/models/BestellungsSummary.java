9221285e1b372123fa1aaae079a7163b9b2bd90c
matlorr <matvey.lorkish@outlook.de>
2020-02-24 14:38:09 +0100

    ADD BestellungsSummary models

--- src/main/java/de/bricks/brickmarket/models/BestellungsSummary.java
+++ src/main/java/de/bricks/brickmarket/models/BestellungsSummary.java
@@ -1,4 +1,12 @@
 package de.bricks.brickmarket.models;
 
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
 public class BestellungsSummary {
+    private long nr;
+    private LocalDate datum;
+    private String kunde;
 }
