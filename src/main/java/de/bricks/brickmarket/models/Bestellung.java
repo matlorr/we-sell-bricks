8064913a204cb2f574b8e2ab50f5cc43ad1accdd
matlorr <matvey.lorkish@outlook.de>
2020-02-24 14:04:10 +0100

    FINISHED Bestellung model

--- src/main/java/de/bricks/brickmarket/models/Bestellung.java
+++ src/main/java/de/bricks/brickmarket/models/Bestellung.java
@@ -1,4 +1,15 @@
 package de.bricks.brickmarket.models;
 
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Value
 public class Bestellung {
+    private Long id;
+    private LocalDate date;
+    private Kunde kunde;
+    private List<BestellungsPosition> positionen;
+
 }
