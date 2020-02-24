46659516de51b2ae1e9d34bc8892bc5b8c87c39f
Tosch160 <torben-g-schmitz@gmx.de>
2020-02-24 14:57:21 +0100

    Controller Annotation

--- src/main/java/de/bricks/brickmarket/web/RechnungsController.java
+++ src/main/java/de/bricks/brickmarket/web/RechnungsController.java
@@ -4,12 +4,14 @@ import de.bricks.brickmarket.ModelService;
 import de.bricks.brickmarket.models.BestellungsPosition;
 import de.bricks.brickmarket.models.BestellungsSummary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class RechnungsController{
     ArrayList<BestellungsPosition> positionen = new ArrayList<BestellungsPosition>();
     @Autowired
@@ -17,7 +19,7 @@ public class RechnungsController{
     @GetMapping("/")
     public String index(Model model){
         List<BestellungsSummary> summaries = modelTranslator.alleBestellungen();
-        model.addAttribute("bestellung",summaries);
+        model.addAttribute("bestellungen",summaries);
         System.out.println(summaries);
         return "uebersicht";
     }
